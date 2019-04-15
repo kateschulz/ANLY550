88d2f3c0a67b02de0fe7ba452707ce25675b9440
kateschulz <ks1533@georgetown.edu>
2019-04-15 10:36:25 -0400

    Update avg_MST.py

--- avg_MST.py
+++ avg_MST.py
@@ -120,7 +120,7 @@ def prims(V, G):
   return MST
 
 from cachetools import cached, TTLCache  
-cache = TTLCache(maxsize=10000, ttl = 1000)  
+cache = TTLCache(maxsize=100000, ttl = 10)  
 @cached(cache)
 
 # Run Prim's algorithm to compute the average MST weight from n vertices in a given 
